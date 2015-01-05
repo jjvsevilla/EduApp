1b00d1587adf02ff6d63543f79060b32e434431c
jjvsevilla <j.j.v.sevilla@hotmail.com>
2015-01-04 21:57:34 -0500

    add new smoke and wip test

--- EduApp.UnitTest/ExamManagerTests.cs
+++ EduApp.UnitTest/ExamManagerTests.cs
@@ -7,10 +7,24 @@ namespace EduApp.UnitTest
     public class ExamManagerTests
     {
         //[Test]
+        [Category("wip")]
         [TestCase("ingles", "ingenieria de sistemas", true)]
         [TestCase("mate", "", false)]
         [TestCase("mate 2", "ingenieria de sistemas", true)]
         public void ShouldValidateExamInputs(string curso, string carrera, bool expectedResult)
+        {
+            var sut = new ExamManager();
+            var actualResult = sut.ValidityOf(curso, carrera);
+            //Assert.That(isValid, Is.True);
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        //[Test]
+        [Category("smoke")]
+        [TestCase("ingles avanzado", "ingenieria de sistemas", true)]
+        [TestCase("mate 3", "", false)]
+        [TestCase("mate 4", "ingenieria de sistemas", true)]
+        public void ShouldValidateExamInputsToo(string curso, string carrera, bool expectedResult)
         {
             var sut = new ExamManager();
             var actualResult = sut.ValidityOf(curso, carrera);
